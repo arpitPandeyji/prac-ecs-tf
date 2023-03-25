6566ff90e217983bf8ccd06e0be1ad5e17bca20f
arpitPandeyji <128712087+arpitPandeyji@users.noreply.github.com>
2023-03-25 18:25:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,5 +8,10 @@ terraform {
 #         workspaces {
 #           name = "ecs-fargate-github"
 #         }
+resource "null_resource" "example" {
+#       triggers = {
+#         value = "A example resource that does nothing!"
+#       }
+#     }
 #       
 # }
