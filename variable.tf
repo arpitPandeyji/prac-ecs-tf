e9c1eeef8d1e95ccd039e2f7a45aef90ddb0966a
arpitPandeyji <128712087+arpitPandeyji@users.noreply.github.com>
2023-03-26 01:54:01 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -72,8 +72,8 @@ variable "aws_ecr_repository" {
 
 variable "aws_ecs_task_def_fam" {
   type        = string
-  description = "demo_ecs_fam"
-  default     = "demo_ecs_fam"
+  description = "demo_ecs_fam-1"
+  default     = "demo_ecs_fam-1"
 }
 
 variable "fargate_cpu" {
@@ -91,7 +91,7 @@ variable "fargate_memory" {
 variable "aws_ecs_service_name" {
   type        = string
   description = "service name"
-  default     = "demo_ecs_svc"
+  default     = "demo_ecs_svc-1"
 }
 
 variable "app_port" {
