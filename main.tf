bb19541dbbfe50c5a19aa4e7098f76949ca7406c
arpitPandeyji <128712087+arpitPandeyji@users.noreply.github.com>
2023-03-25 18:09:53 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+terraform {
+#       backend "remote" {
+#         # The name of your Terraform Cloud organization.
+#         organization = "gh-task"
+#
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+#         workspaces {
+#           name = "ecs-fargate-github"
+#         }
+#       }
+#     }
