ee6c2e7ec847e8562bcd2280d3f345f618bb6365
arpitPandeyji <128712087+arpitPandeyji@users.noreply.github.com>
2023-03-25 18:13:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,11 @@ terraform {
 #       backend "remote" {
 #         # The name of your Terraform Cloud organization.
 #         organization = "gh-task"
+          }
 #
 #         # The name of the Terraform Cloud workspace to store Terraform state files in.
 #         workspaces {
 #           name = "ecs-fargate-github"
 #         }
-#       }
-#     }
+#       
+# }
