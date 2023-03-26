1ad8a82349d8bab9259b16528f92fdc00f7b7c1e
arpitPandeyji <128712087+arpitPandeyji@users.noreply.github.com>
2023-03-26 16:11:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,23 @@
 var express = require('express');
 var app = express();
 
+app.set("port", process.env.PORT || 80);
+
 app.get('/', function(req, res){
   res.send('Hello World');
 });
 
 /* istanbul ignore next */
-if (!module.parent) {
+/*if (!module.parent) {
   app.listen(3000);
   console.log('Express started on port 3000');
 }
+*/
+var server = app.listen(app.get("port"), function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Node.js API app listening at http://%s:%s", host, port)
+
+})
